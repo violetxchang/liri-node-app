ca42b9422e27562310c68b888a94670266aca81f
violet chang <violet7chang@gmail.com>
2019-06-30 22:11:25 -0500

    update

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 console.log('this is loaded');
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+  id: process.env.41b63d87a9b542e491f5c6c98f0d6879,
+  secret: process.env.52ba7dbc205e4fcca481859093cd9fe0
 };

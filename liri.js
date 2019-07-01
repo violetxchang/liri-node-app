0fc3891642946adee2598409628d49019d5efc55
violet chang <violet7chang@gmail.com>
2019-07-01 12:52:04 -0500

    more updates to everything

---
+++ liri.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var spotify = new Spotify(keys.spotify);
+
+// Basic Node application for requesting data from the OMDB website via axios
+// Here we incorporate the "axios" npm package
+var axios = require("axios");
+
+// We then run the request with axios module on a URL with a JSON
+axios.get("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy").then(
+  function(response) {
+    // Then we print out the imdbRating
+    console.log("The movie's rating is: " + response.data.imdbRating);
+  }
+);

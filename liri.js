66363573f5c9e6674ef0064c3e761509acdb2a20
violet chang <violet7chang@gmail.com>
2019-07-06 18:01:02 -0500

    movie-this added

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var key = require("./keys");
+var moment = require("moment");
 
 // Include the axios npm package (Don't forget to run "npm install axios" in this folder first!)
 var axios = require("axios");
@@ -27,6 +28,45 @@ switch (action) {
 
     default:
         console.log("No such value found.")
+}
+
+//#1
+//#3 
+
+function concertThis(Artist){
+
+// Then run a request with axios to the OMDB API with the movie specified
+var queryUrl = "https://rest.bandsintown.com/artists/" + Artist + "/events?app_id=codingbootcamp";
+
+axios.get(queryUrl).then(
+        function(response){
+            console.log("-----------------------------------------------------------------");
+            console.log("Venue Name: "+response.data[0].venue.name);
+            console.log("Venue Location: "+response.data[0].venue.city+", "+response.data[0].venue.country);
+            var dateRev=moment(response.data[0].datetime).format("L");
+            console.log("Date of Event: "+dateRev);
+            console.log("-----------------------------------------------------------------");
+        })
+    .catch(function (error) {
+        if (error.response) {
+            // The request was made and the server responded with a status code
+            // that falls out of the range of 2xx
+            console.log("---------------Data---------------");
+            console.log(error.response.data);
+            console.log("---------------Status---------------");
+            console.log(error.response.status);
+            console.log("---------------Status---------------");
+            console.log(error.response.headers);
+        } else if (error.request) {
+            // The request was made but no response was received
+            // `error.request` is an object that comes back with details pertaining to the error that occurred.
+            console.log(error.request);
+        } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log("Error", error.message);
+        }
+        console.log(error.config);
+    });
 }
 
 
